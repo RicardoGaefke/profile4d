8620747e0f9bb34ff64c88d07674b4af9f38f0ec
Ricardo Gaefke <ricardogaefke@gmail.com>
2019-11-18 12:58:33 -0300

    Web.Site DataProtection ok

--- src/Web.Site/Startup.cs
+++ src/Web.Site/Startup.cs
@@ -10,48 +10,50 @@ using Profile4d.DI;
 
 namespace Web.Site
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration, IHostEnvironment environment)
     {
-        public Startup(IConfiguration configuration, IHostEnvironment environment)
-        {
-            Configuration = configuration;
-            HostingEnvironment = environment;
-        }
-
-        public IConfiguration Configuration { get; }
-        public IHostEnvironment HostingEnvironment { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddRazorPages();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-            });
-        }
+        Configuration = configuration;
+        HostingEnvironment = environment;
     }
+
+    public IConfiguration Configuration { get; }
+    public IHostEnvironment HostingEnvironment { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      // Add your AppInsights ID here to make it globally available //
+      // services.AddApplicationInsightsTelemetry("465f47b3-8d7a-46ee-a81e-e51182c12296");
+        
+        services.AddRazorPages();
+        Bootstrap.DataProtection(services, Configuration);
+    }
+
+    public void Configure(IApplicationBuilder app, IHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapRazorPages();
+        });
+    }
+  }
 }
