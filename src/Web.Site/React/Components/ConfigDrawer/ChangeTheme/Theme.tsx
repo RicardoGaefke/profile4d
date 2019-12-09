9b4e1d86b0a0ad9217329e8b21d3223b63638d25
Ricardo Gaefke <ricardogaefke@gmail.com>
2019-12-09 12:40:15 -0300

    Web.Site Config Theme language

--- src/Web.Site/React/Components/ConfigDrawer/ChangeTheme/Theme.tsx
+++ src/Web.Site/React/Components/ConfigDrawer/ChangeTheme/Theme.tsx
@@ -1,34 +1,53 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import i18next from 'i18next';
 import {
   Radio, RadioGroup, FormControl, FormLabel, FormControlLabel, Divider,
 } from '@material-ui/core';
+// eslint-disable-next-line no-unused-vars
+import { withTranslation, WithTranslation } from 'react-i18next';
 import { useStateValue } from '../../../Initial/Context/StateProvider';
 import styles from '../Styles';
+import LanguagePT from './LanguagePT';
+import LanguageENG from './LanguageENG';
 
-export default (): React.ReactElement<any> => {
-  const [{ Theme }, dispatch] = useStateValue();
+export default withTranslation()(
+  (props: WithTranslation): React.ReactElement<WithTranslation> => {
+    const { t } = props;
+    const [{ Theme, Language }, dispatch] = useStateValue();
+    const classes = styles({});
 
-  const classes = styles({});
+    useEffect((): void => {
+      i18next.changeLanguage(Language);
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    dispatch({
-      type: 'changeTheme',
-      value: (event.target as HTMLInputElement).value,
-    });
-  };
+      i18next.addResourceBundle('PT', 'ThemeConfig', LanguagePT);
+      i18next.addResourceBundle('ENG', 'ThemeConfig', LanguageENG);
+      // return type void != (): void... so as unknown as void
+      return ((): void => {
+        i18next.removeResourceBundle('PT', 'ThemeConfig');
+        i18next.removeResourceBundle('ENG', 'ThemeConfig');
+      }) as unknown as void;
+    }, []);
 
-  return (
-    <>
-      <FormControl component="fieldset">
-        <FormLabel component="legend">
-          Escolha o tema
-        </FormLabel>
-        <RadioGroup aria-label="gender" name="gender1" value={Theme} onChange={handleChange}>
-          <FormControlLabel value="dark" control={<Radio />} label="Dark" />
-          <FormControlLabel value="light" control={<Radio />} label="Light" />
-        </RadioGroup>
-      </FormControl>
-      <Divider className={classes.divider} />
-    </>
-  );
-};
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+      dispatch({
+        type: 'changeTheme',
+        value: (event.target as HTMLInputElement).value,
+      });
+    };
+
+    return (
+      <>
+        <FormControl component="fieldset">
+          <FormLabel component="legend">
+            {t('ThemeConfig:title')}
+          </FormLabel>
+          <RadioGroup aria-label="gender" name="gender1" value={Theme} onChange={handleChange}>
+            <FormControlLabel value="dark" control={<Radio />} label={t('ThemeConfig:dark')} />
+            <FormControlLabel value="light" control={<Radio />} label={t('ThemeConfig:light')} />
+          </RadioGroup>
+        </FormControl>
+        <Divider className={classes.divider} />
+      </>
+    );
+  },
+);
