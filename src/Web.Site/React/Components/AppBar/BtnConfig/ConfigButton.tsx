f0a2e78ba21274253019e5f5eee1f7386e417fcc
Ricardo Gaefke <ricardogaefke@gmail.com>
2019-12-09 15:12:15 -0300

    Web.Site multilanguage template

--- src/Web.Site/React/Components/AppBar/BtnConfig/ConfigButton.tsx
+++ src/Web.Site/React/Components/AppBar/BtnConfig/ConfigButton.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import i18next from 'i18next';
+// eslint-disable-next-line no-unused-vars
+import { withTranslation, WithTranslation } from 'react-i18next';
 import TuneIcon from '@material-ui/icons/Tune';
 import { useStateValue } from '../../../Initial/Context/StateProvider';
 import Button from '../Button/Button';
+import LanguagePT from './LanguagePT';
+import LanguageENG from './LanguageENG';
 
-export default (): React.ReactElement<any> => {
-  const [{ Config }, dispatch] = useStateValue();
+export default withTranslation()(
+  (props: WithTranslation): React.ReactElement<WithTranslation> => {
+    const { t } = props;
+    const [{ Config, Language }, dispatch] = useStateValue();
 
-  const toggleConfig = (): void => {
-    dispatch({
-      type: 'changeConfig',
-      value: !Config,
-    });
-  };
+    const toggleConfig = (): void => {
+      dispatch({
+        type: 'changeConfig',
+        value: !Config,
+      });
+    };
 
-  return (
-    <Button
-      color="inherit"
-      title="Alterar configurações do site"
-      onClick={toggleConfig}
-    >
-      <TuneIcon />
-    </Button>
-  );
-};
+    useEffect((): void => {
+      i18next.changeLanguage(Language);
+
+      i18next.addResourceBundle('PT', 'CustomBtnConfig', LanguagePT);
+      i18next.addResourceBundle('ENG', 'CustomBtnConfig', LanguageENG);
+      // return type void != (): void... so as unknown as void
+      return ((): void => {
+        i18next.removeResourceBundle('PT', 'CustomBtnConfig');
+        i18next.removeResourceBundle('ENG', 'CustomBtnConfig');
+      }) as unknown as void;
+    }, []);
+
+    return (
+      <Button
+        color="inherit"
+        title={t('CustomBtnConfig:title')}
+        onClick={toggleConfig}
+      >
+        <TuneIcon />
+      </Button>
+    );
+  },
+);
--- src/Web.Site/React/Components/AppBar/BtnLogin/LoginButton.tsx
+++ src/Web.Site/React/Components/AppBar/BtnLogin/LoginButton.tsx
@@ -1,27 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import i18next from 'i18next';
+// eslint-disable-next-line no-unused-vars
+import { withTranslation, WithTranslation } from 'react-i18next';
 import PermIdentityIcon from '@material-ui/icons/PermIdentity';
 import { useStateValue } from '../../../Initial/Context/StateProvider';
 import { Href } from '../../../Utils/Domain';
 import Button from '../Button/Button';
+import LanguagePT from './LanguagePT';
+import LanguageENG from './LanguageENG';
 
-export default (): React.ReactElement<any> => {
-  const [{ Language }] = useStateValue();
+export default withTranslation()(
+  (props: WithTranslation): React.ReactElement<WithTranslation> => {
+    const { t } = props;
+    const [{ Language }] = useStateValue();
 
-  const myLogin = (): void => {
-    // const url = new Href(window.location.href.replace(/https?:\/\//i, ''));
-    const url = new Href('staging.www.ricardogaefke.com');
-    // window.location.href = url.toLogin();
-    // eslint-disable-next-line no-console
-    console.log(url.toLogin());
-  };
+    const myLogin = (): void => {
+      // const url = new Href(window.location.href.replace(/https?:\/\//i, ''));
+      const url = new Href('staging.www.ricardogaefke.com');
+      // window.location.href = url.toLogin();
+      // eslint-disable-next-line no-console
+      console.log(url.toLogin());
+    };
 
-  return (
-    <Button
-      color="inherit"
-      title={`Manage identity ${Language}`}
-      onClick={myLogin}
-    >
-      <PermIdentityIcon />
-    </Button>
-  );
-};
+    useEffect((): void => {
+      i18next.changeLanguage(Language);
+
+      i18next.addResourceBundle('PT', 'LoginBtnConfig', LanguagePT);
+      i18next.addResourceBundle('ENG', 'LoginBtnConfig', LanguageENG);
+      // return type void != (): void... so as unknown as void
+      return ((): void => {
+        i18next.removeResourceBundle('PT', 'LoginBtnConfig');
+        i18next.removeResourceBundle('ENG', 'LoginBtnConfig');
+      }) as unknown as void;
+    }, []);
+
+    return (
+      <Button
+        color="inherit"
+        title={t('LoginBtnConfig:title')}
+        onClick={myLogin}
+      >
+        <PermIdentityIcon />
+      </Button>
+    );
+  },
+);
