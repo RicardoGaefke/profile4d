01ee55a17be9cac13d8cb5e852d944b3dab848f2
Ricardo Gaefke <ricardogaefke@gmail.com>
2019-12-10 16:06:12 -0300

    Web.Site About

---
+++ src/Web.Site/React/Components/About/Main/Main.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react';
+import i18next from 'i18next';
+// eslint-disable-next-line no-unused-vars
+import { withTranslation, WithTranslation } from 'react-i18next';
+import {
+  Typography, Container,
+} from '@material-ui/core';
+import { useStateValue } from '../../../Initial/Context/StateProvider';
+import useStyles from './Styles';
+import './Language';
+import LanguagePT from './LanguagePT';
+import LanguageENG from './LanguageENG';
+
+export default withTranslation()(
+  (props: WithTranslation): React.ReactElement<WithTranslation> => {
+    const { t } = props;
+    const [{ Language }] = useStateValue();
+    const classes = useStyles({});
+
+    useEffect((): void => {
+      if (!i18next.hasResourceBundle('PT', 'AboutMain')) {
+        i18next.addResourceBundle('PT', 'AboutMain', LanguagePT);
+      }
+      if (!i18next.hasResourceBundle('ENG', 'AboutMain')) {
+        i18next.addResourceBundle('ENG', 'AboutMain', LanguageENG);
+      }
+      i18next.changeLanguage(Language);
+      // return type void != (): void... so as unknown as void
+      return ((): void => {
+        i18next.removeResourceBundle('PT', 'AboutMain');
+        i18next.removeResourceBundle('ENG', 'AboutMain');
+      }) as unknown as void;
+    }, []);
+
+    return (
+      <div className={classes.main}>
+        <img alt={t('AboutMain:title')} src="/img/team.jpg" className={classes.img} />
+        <Container maxWidth="md">
+          <Typography variant="h1" align="center" gutterBottom>
+            {t('AboutMain:title')}
+          </Typography>
+          <Typography variant="overline" className={classes.text} component="p">
+            {t('AboutMain:text')}
+          </Typography>
+        </Container>
+      </div>
+    );
+  },
+);
--- src/Web.Site/React/Views/About/About.tsx
+++ src/Web.Site/React/Views/About/About.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
+import Main from '../../Components/About/Main/Main';
 
 export default (): React.ReactElement<any> => (
-  <p>
-    About
-  </p>
+  <Main />
 );
