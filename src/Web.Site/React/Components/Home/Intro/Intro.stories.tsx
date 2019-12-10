5338c8751106263457c0ce7c72ac5d2ebcca1961
Ricardo Gaefke <ricardogaefke@gmail.com>
2019-12-10 13:31:19 -0300

    Web.Site Intro

--- src/Web.Site/React/Components/Home/Intro/Intro.stories.tsx
+++ src/Web.Site/React/Components/Home/Intro/Intro.stories.tsx
@@ -5,15 +5,15 @@ import { withConsole } from '@storybook/addon-console';
 // eslint-disable-next-line no-unused-vars
 import { action, HandlerFunction } from '@storybook/addon-actions';
 import MyThemeHoc from '../../../Initial/Theme/StoryBookHOC';
-import Banner from './Banner';
+import Intro from './Intro';
 
 const App = (): React.ReactElement => (
   <MyThemeHoc>
-    <Banner />
+    <Intro />
   </MyThemeHoc>
 );
 
-storiesOf('Home-Banner', module)
+storiesOf('Home-Intro', module)
   .addDecorator((story: any): React.ReactElement => (
     <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
   ))
--- src/Web.Site/React/Views/Home/Home.tsx
+++ src/Web.Site/React/Views/Home/Home.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Divider } from '@material-ui/core';
 import Home from '../../Components/Home/Banner/Banner';
+import Intro from '../../Components/Home/Intro/Intro';
 
 export default (): React.ReactElement<any> => (
   <>
     <Home />
+    <Intro />
+    <Divider />
   </>
 );
