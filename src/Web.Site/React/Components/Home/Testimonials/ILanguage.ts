77159036b41338c70ed6c04b170efd2947973963
Ricardo Gaefke <ricardogaefke@gmail.com>
2019-12-10 15:12:39 -0300

    Web.Site Testimonials

---
+++ src/Web.Site/React/Components/Home/Testimonials/ILanguage.ts
@@ -0,0 +1,10 @@
+export interface ILanguage {
+  text1: ITestimonial,
+  text2: ITestimonial,
+  text3: ITestimonial,
+}
+
+interface ITestimonial {
+  text: string,
+  author: string,
+}
--- src/Web.Site/React/Views/Home/Home.tsx
+++ src/Web.Site/React/Views/Home/Home.tsx
@@ -3,6 +3,7 @@ import { Divider } from '@material-ui/core';
 import Home from '../../Components/Home/Banner/Banner';
 import Intro from '../../Components/Home/Intro/Intro';
 import Announcements from '../../Components/Home/Announcements/Announcements';
+import Testimonials from '../../Components/Home/Testimonials/Testimonials';
 
 export default (): React.ReactElement<any> => (
   <>
@@ -11,5 +12,6 @@ export default (): React.ReactElement<any> => (
     <Divider />
     <Announcements />
     <Divider />
+    <Testimonials />
   </>
 );
