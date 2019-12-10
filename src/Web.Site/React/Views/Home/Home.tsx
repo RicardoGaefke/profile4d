9f1b2b215a67a726714ed31359c67f01bb818da9
Ricardo Gaefke <ricardogaefke@gmail.com>
2019-12-10 14:28:35 -0300

    Web.Site Announcements

--- src/Web.Site/React/Views/Home/Home.tsx
+++ src/Web.Site/React/Views/Home/Home.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { Divider } from '@material-ui/core';
 import Home from '../../Components/Home/Banner/Banner';
 import Intro from '../../Components/Home/Intro/Intro';
+import Announcements from '../../Components/Home/Announcements/Announcements';
 
 export default (): React.ReactElement<any> => (
   <>
     <Home />
     <Intro />
+    <Divider />
+    <Announcements />
     <Divider />
   </>
 );
