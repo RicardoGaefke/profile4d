688b06b976bd79f808db1b02e5f85d23f413d1da
Ricardo Gaefke <ricardogaefke@gmail.com>
2019-12-13 18:09:18 -0300

    Web.Site NodeServices config

--- src/Web.Site/Startup.cs
+++ src/Web.Site/Startup.cs
@@ -43,8 +43,8 @@ namespace Profile4d.Web.Site
         {
           if (HostingEnvironment.IsDevelopment())
           {
-            // options.ProjectPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
-            options.ProjectPath = Directory.GetCurrentDirectory();
+            options.ProjectPath = Path.Combine(Directory.GetCurrentDirectory(), "site/wwwroot");
+            // options.ProjectPath = Directory.GetCurrentDirectory();
             // options.ProjectPath = Path.GetFullPath("/usr/local/bin/node");
           }
         }
