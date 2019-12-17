f171293c146d95db59722bb9ad1041f2f1beb066
LNeeevs <lucasneves.dev@gmail.com>
2019-12-17 15:38:39 -0300

    Footer.test padronized too

--- src/Web.Site/React/Components/Footer/Footer.test.tsx
+++ src/Web.Site/React/Components/Footer/Footer.test.tsx
@@ -10,15 +10,19 @@ chai.use(chaiEnzyme());
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Footer', (): void => {
-  it('Should exist Footer', (): void => {
-    const wrapper = shallow(<MyFooter />);
-    // eslint-disable-next-line no-unused-expressions
-    expect(wrapper).to.exist;
+  describe('Smoke Tests', (): void => {
+    it('Should exist Footer', (): void => {
+      const wrapper = shallow(<MyFooter />);
+      // eslint-disable-next-line no-unused-expressions
+      expect(wrapper).to.exist;
+    });
   });
-  it('Should Footer component have a children equal Copyright when is called', (): void => {
-    const wrapper = shallow(<MyFooter />);
-    const date: Date = new Date();
-    const copyright: string = `Profile4D ©, ,${date.getFullYear()}, ,by Ricardo Gaefke`;
-    expect(wrapper.find(Typography).props().children?.toString()).equal(copyright);
+  describe('Childrens Tests', (): void => {
+    it('Should Footer component have a children equal Copyright when is called', (): void => {
+      const wrapper = shallow(<MyFooter />);
+      const date: Date = new Date();
+      const copyright: string = `Profile4D ©, ,${date.getFullYear()}, ,by Ricardo Gaefke`;
+      expect(wrapper.find(Typography).props().children?.toString()).equal(copyright);
+    });
   });
 });
