fc84fbd6ffbff7df400f17232140c480c1e9d0a5
LNeeevs <lucasneves.dev@gmail.com>
2019-12-20 00:08:43 -0300

    Banner.test created with success, go build

--- src/Web.Site/React/Components/Home/Banner/Banner.test.tsx
+++ src/Web.Site/React/Components/Home/Banner/Banner.test.tsx
@@ -7,19 +7,19 @@ import appData from '../../../Initial/Context/InitialContext';
 import MyStateProvider from '../../../Initial/Context/AppContext';
 import MyApp from '../../../Initial/Tests/TestsApp';
 import Banner from './Banner';
+import { Button } from '@material-ui/core';
 
 chai.use(chaiEnzyme());
 Enzyme.configure({ adapter: new Adapter() });
 
-const App = (): React.ReactElement<any> => (
-  <MyStateProvider initialContext={appData}>
-    <MyApp>
-      <Banner />
-    </MyApp>
-  </MyStateProvider>
-);
-
 describe('Home.Banner', (): void => {
+  const App = (): React.ReactElement<any> => (
+    <MyStateProvider initialContext={appData}>
+      <MyApp>
+        <Banner />
+      </MyApp>
+    </MyStateProvider>
+  );
   describe('Smoke Tests', (): void => {
     it('Should exist Banner', (): void => {
       const wrapper = shallow(<App />);
@@ -28,13 +28,48 @@ describe('Home.Banner', (): void => {
     });
   });
   describe('InitialContext Provider', (): void => {
-    it('Should return Theme like "light" by default', (): void => {
+    it('Should return App with Ready context like "false" by default', (): void => {
+      const wrapper = shallow(<App />);
+      expect(wrapper.props().initialContext.Ready).equal(false);
+    });
+    it('Should return App with Theme context like "light" by default', (): void => {
       const wrapper = shallow(<App />);
       expect(wrapper.props().initialContext.Theme).equal('light');
     });
-    it('Should return Language like "PT" by default', (): void => {
+    it('Should return App with Language context like "PT" by default', (): void => {
       const wrapper = shallow(<App />);
       expect(wrapper.props().initialContext.Language).equal('PT');
     });
+    it('Should return App with ConsentCookie context like "true" by default', (): void => {
+      const wrapper = shallow(<App />);
+      expect(wrapper.props().initialContext.ConsentCookie).equal(true);
+    });
+    it('Should return App with Name context like "Profile4d" by default', (): void => {
+      const wrapper = shallow(<App />);
+      expect(wrapper.props().initialContext.Name).equal('Profile4d');
+    });
+    it('Should return App with Email context like "empty" by default', (): void => {
+      const wrapper = shallow(<App />);
+      expect(wrapper.props().initialContext.Email).equal('');
+    });
+    it('Should return App with KeepConnected context like "false" by default', (): void => {
+      const wrapper = shallow(<App />);
+      expect(wrapper.props().initialContext.KeepConnected).equal(false);
+    });
+    it('Should return App with Drawer context like "false" by default', (): void => {
+      const wrapper = shallow(<App />);
+      expect(wrapper.props().initialContext.Drawer).equal(false);
+    });
+    it('Should return App with IsAuthenticated context like "false" by default', (): void => {
+      const wrapper = shallow(<App />);
+      expect(wrapper.props().initialContext.IsAuthenticated).equal(false);
+    });
   });
+  // describe('Childrens Return', (): void => {
+  //   it('Should exist Typography', (): void => {
+  //     const wrapper = shallow(<App />);
+  //     const myTests = wrapper.find(Banner);
+  //     expect(myTests.find('#myBanner').props().children).to.be.equal('');
+  //   });
+  // });
 });
