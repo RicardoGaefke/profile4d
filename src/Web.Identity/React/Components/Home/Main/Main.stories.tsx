13d7649eb8ea001f9e9192f82425a2243f60bcf3
Ricardo Gaefke <ricardogaefke@gmail.com>
2019-12-27 14:31:03 -0300

    Web.Identity Storibook for login router

--- src/Web.Identity/React/Components/Home/Main/Main.stories.tsx
+++ src/Web.Identity/React/Components/Home/Main/Main.stories.tsx
@@ -5,19 +5,30 @@ import { withConsole } from '@storybook/addon-console';
 // eslint-disable-next-line no-unused-vars
 import { action, HandlerFunction } from '@storybook/addon-actions';
 import MyThemeHoc from '../../../Initial/Theme/StoryBookHOC';
-import Main from './Main';
+import MyApp from '../../../Initial/App';
 
 const App = (): React.ReactElement => (
   <MyThemeHoc>
-    <Main />
+    <MyApp />
   </MyThemeHoc>
 );
 
-storiesOf('Login form', module)
+storiesOf('Forms', module)
   .addDecorator((story: any): React.ReactElement => (
     <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
   ))
   .addDecorator((storyFn, context): React.ReactElement => withConsole()(storyFn)(context))
-  .add('Basic', (): React.ReactElement => (
+  .add('Login', (): React.ReactElement => (
     <App />
+  ));
+
+storiesOf('Forms', module)
+  .addDecorator((storyFn, context): React.ReactElement => withConsole()(storyFn)(context))
+  .add('Create User', (): React.ReactElement => (
+    <MemoryRouter
+      initialEntries={['/', '/account/', '/account/create/']}
+      initialIndex={2}
+    >
+      <App />
+    </MemoryRouter>
   ));
