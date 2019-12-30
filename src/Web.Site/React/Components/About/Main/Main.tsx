2e094094f09a0a74c8314765bb09a49ecf3098da
LNeeevs <lucasneves.dev@gmail.com>
2019-12-30 18:42:11 -0300

    About with setLanguage

--- src/Web.Site/React/Components/About/Main/Main.tsx
+++ src/Web.Site/React/Components/About/Main/Main.tsx
@@ -1,33 +1,17 @@
-import React, { useEffect } from 'react';
-import i18next from 'i18next';
+import React from 'react';
 // eslint-disable-next-line no-unused-vars
 import { withTranslation, WithTranslation } from 'react-i18next';
 import {
   Typography, Container,
 } from '@material-ui/core';
 import useStyles from './Styles';
-import './Language';
-import LanguagePT from './LanguagePT';
-import LanguageENG from './LanguageENG';
+import setLanguage from './Language';
 
 export default withTranslation()(
   (props: WithTranslation): React.ReactElement<WithTranslation> => {
     const { t } = props;
     const classes = useStyles({});
-
-    useEffect((): void => {
-      if (!i18next.hasResourceBundle('PT', 'AboutMain')) {
-        i18next.addResourceBundle('PT', 'AboutMain', LanguagePT);
-      }
-      if (!i18next.hasResourceBundle('ENG', 'AboutMain')) {
-        i18next.addResourceBundle('ENG', 'AboutMain', LanguageENG);
-      }
-      // return type void != (): void... so as unknown as void
-      return ((): void => {
-        i18next.removeResourceBundle('PT', 'AboutMain');
-        i18next.removeResourceBundle('ENG', 'AboutMain');
-      }) as unknown as void;
-    }, []);
+    setLanguage();
 
     return (
       <div className={classes.main}>
