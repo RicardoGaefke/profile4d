fd394eaaacbf7278fa038bf95df3811709bd9ae8
LNeeevs <lucasneves.dev@gmail.com>
2020-01-21 14:51:12 -0300

    Nested router and forms removed with success

--- src/Web.Admin/React/Components/Home/Main/Main.stories.tsx
+++ src/Web.Admin/React/Components/Home/Main/Main.stories.tsx
@@ -13,33 +13,11 @@ const App = (): React.ReactElement => (
   </MyThemeHoc>
 );
 
-storiesOf('Forms', module)
+storiesOf('Dashboard', module)
   .addDecorator((story: any): React.ReactElement => (
     <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
   ))
   .addDecorator((storyFn, context): React.ReactElement => withConsole()(storyFn)(context))
-  .add('Login', (): React.ReactElement => (
+  .add('Admin', (): React.ReactElement => (
     <App />
-  ));
-
-/* this stories has double header and footer because I imported HOC + App to use full router
-I don't mind as I just want to see the form */
-storiesOf('Forms', module)
-  .addDecorator((storyFn, context): React.ReactElement => withConsole()(storyFn)(context))
-  .add('Create User', (): React.ReactElement => (
-    <MemoryRouter
-      initialEntries={['/account/create/']}
-    >
-      <App />
-    </MemoryRouter>
-  ));
-
-storiesOf('Forms', module)
-  .addDecorator((storyFn, context): React.ReactElement => withConsole()(storyFn)(context))
-  .add('Forgot Form', (): React.ReactElement => (
-    <MemoryRouter
-      initialEntries={['/account/forgot/']}
-    >
-      <App />
-    </MemoryRouter>
   ));
