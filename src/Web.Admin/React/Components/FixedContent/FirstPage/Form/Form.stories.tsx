fa85d643b2e3dc57c67229f214c8832f85ff6a80
lucasneves.dev@gmail.com <lucasneves.dev@gmail.com>
2020-01-30 12:12:27 -0300

    FixedContent.FirstPage now with form storybook

--- src/Web.Admin/React/Components/FixedContent/FirstPage/Form/Form.stories.tsx
+++ src/Web.Admin/React/Components/FixedContent/FirstPage/Form/Form.stories.tsx
@@ -1,26 +1,9 @@
 import React from 'react';
-// import { withFormik } from 'formik';
 import { storiesOf } from '@storybook/react';
 import { withConsole } from '@storybook/addon-console';
 import { MemoryRouter } from 'react-router';
 import MyThemeHOC from '../../../../Initial/Theme/StoryBookHOC';
-// // eslint-disable-next-line no-unused-vars
-// import { IStaticFirstPage } from '../../../../../../TypeScript/Interfaces/IStaticContent';
-// import initialValues from './Context/InitialValues';
-// import useStyles from './Form.Styles';
 import MyForm from './Context/Context';
-
-// const MyNewForm = withFormik({
-//   displayName: 'Form',
-//   enableReinitialize: true,
-//   mapPropsToValues: (): IStaticFirstPage => (initialValues),
-//   handleSubmit: (values: IStaticFirstPage, { resetForm, setSubmitting }): void => {
-//     // eslint-disable-next-line no-alert
-//     alert(JSON.stringify(values));
-//     setSubmitting(false);
-//     resetForm();
-//   },
-// })(MyForm);
 
 const App = (): React.ReactElement => (
   <MyThemeHOC>
