f57ff85ae166fb5260d1d3874126ce2bbb06140d
Ricardo Gaefke <ricardogaefke@gmail.com>
2020-01-30 19:40:30 -0300

    Web.Admin Node nointeractive

--- src/Web.Admin/start1.sh
+++ src/Web.Admin/start1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 apt-get install curl
-curl -sL https://deb.nodesource.com/setup_12.6 | bash
-apt-get install -y nodejs
+curl -sL https://deb.nodesource.com/setup_12.x | bash
+DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs
 
 set -e
 export PORT=8080
