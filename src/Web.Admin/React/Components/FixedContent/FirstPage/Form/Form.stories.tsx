16ad55f11c054fe1a41fea1f749493c2f6ce5a4d
lucasneves.dev@gmail.com <lucasneves.dev@gmail.com>
2020-01-31 15:48:08 -0300

    Rows and MaxRows with 9500 catacteres, DominantProfile and InferiorMind created with success, local story removed

--- src/Web.Admin/React/Components/FixedContent/FirstPage/Form/Form.stories.tsx
+++ src/Web.Admin/React/Components/FixedContent/FirstPage/Form/Form.stories.tsx
@@ -1,31 +1,32 @@
 import React from 'react';
-import { withFormik } from 'formik';
+// import { withFormik } from 'formik';
 import { storiesOf } from '@storybook/react';
 import { withConsole } from '@storybook/addon-console';
 import { MemoryRouter } from 'react-router';
 import MyThemeHOC from '../../../../Initial/Theme/StoryBookHOC';
-// eslint-disable-next-line no-unused-vars
-import { IStaticFirstPage } from '../../../../../../TypeScript/Interfaces/IStaticContent';
-import initialValues from '../DominantProfile/DominantProfile.InitialValues';
-import Validation from './Form.Validation';
+// // eslint-disable-next-line no-unused-vars
+// import { IStaticFirstPage } from '../../../../../../TypeScript/Interfaces/IStaticContent';
+// import initialValues from '../DominantProfile/DominantProfile.InitialValues';
+// import Validation from './Form.Validation';
 import MyForm from './Form.Context';
 
-const SuperForm = withFormik({
-  displayName: 'Form',
-  enableReinitialize: true,
-  mapPropsToValues: (): IStaticFirstPage => (initialValues),
-  validationSchema: Validation,
-  handleSubmit: (values: IStaticFirstPage, { resetForm, setSubmitting }): void => {
-    // eslint-disable-next-line no-alert
-    alert(JSON.stringify(values));
-    setSubmitting(false);
-    resetForm();
-  },
-})(MyForm);
+// Tryng to solve the validationSchema problem here...but it's not functionally yet
+// const SuperForm = withFormik({
+//   displayName: 'Form',
+//   enableReinitialize: true,
+//   mapPropsToValues: (): IStaticFirstPage => (initialValues),
+//   validationSchema: Validation,
+//   handleSubmit: (values: IStaticFirstPage, { resetForm, setSubmitting }): void => {
+//     // eslint-disable-next-line no-alert
+//     alert(JSON.stringify(values));
+//     setSubmitting(false);
+//     resetForm();
+//   },
+// })(MyForm);
 
 const App = (): React.ReactElement => (
   <MyThemeHOC>
-    <SuperForm />
+    <MyForm />
   </MyThemeHOC>
 );
 
@@ -36,7 +37,4 @@ storiesOf('Form', module)
   .addDecorator((storyFn, context): React.ReactElement => withConsole()(storyFn)(context))
   .add('Default', (): React.ReactElement => (
     <App />
-  ))
-  .add('Without context', (): React.ReactElement => (
-    <SuperForm />
   ));
