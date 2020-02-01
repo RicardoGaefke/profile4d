bae352b60e1745ed58f3291c8a17772ca5778ffa
lucasneves.dev@gmail.com <lucasneves.dev@gmail.com>
2020-01-31 21:43:19 -0300

    New form created

---
+++ src/Web.Admin/React/Components/FixedContent/CompetentMind/Form/Form.Validation.ts
@@ -0,0 +1,22 @@
+import * as Yup from 'yup';
+// eslint-disable-next-line no-unused-vars
+import { WithTranslation } from 'react-i18next';
+
+export default (props: WithTranslation): object => Yup.object().shape({
+  Title_PT: Yup.string()
+    .required(props.t('StaticFirstPage:titlePT.required'))
+    .min(5, props.t('StaticFirstPage:titlePT.min'))
+    .max(95, props.t('StaticFirstPage:titlePT.max')),
+  Title_ENG: Yup.string()
+    .required(props.t('StaticFirstPage:titleENG.required'))
+    .min(5, props.t('StaticFirstPage:titleENG.min'))
+    .max(95, props.t('StaticFirstPage:titleENG.max')),
+  Text_PT: Yup.string()
+    .required(props.t('StaticFirstPage:textPT.required'))
+    .min(5, props.t('StaticFirstPage:textPT.min'))
+    .max(9500, props.t('StaticFirstPage:textPT.max')),
+  Text_ENG: Yup.string()
+    .required(props.t('StaticFirstPage:textENG.required'))
+    .min(5, props.t('StaticFirstPage:textENG.min'))
+    .max(9500, props.t('StaticFirstPage:textENG.max')),
+});
