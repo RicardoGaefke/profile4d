78d6d338d628d96ef55a0dd9f817884478490958
Ricardo Gaefke <ricardogaefke@gmail.com>
2020-02-13 18:40:39 -0300

    curl node 12

--- src/Web.Site/start1.sh
+++ src/Web.Site/start1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get install curl
-curl -sL https://deb.nodesource.com/setup_10.x | bash
+curl -sL https://deb.nodesource.com/setup_12.x | bash
 apt-get install -y -qq nodejs
 
 set -e
