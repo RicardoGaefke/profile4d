6b3ba28e5aa96089281666c01be39be9d5b6df9c
lucasneves.dev@gmail.com <lucasneves.dev@gmail.com>
2020-02-16 11:38:15 -0300

    ImageBehavioursProfiles created with success

---
+++ src/Web.Admin/React/Components/Images/ImageBehavioursProfiles/ImageBehavioursProfiles.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+// eslint-disable-next-line no-unused-vars
+import { withTranslation, WithTranslation } from 'react-i18next';
+// eslint-disable-next-line no-unused-vars
+import { withSnackbar, WithSnackbarProps } from 'notistack';
+import { withFormik } from 'formik';
+import { Typography } from '@material-ui/core';
+import useStyles from '../Styles';
+import setLanguage from '../Language';
+import ImageBehavioursProfiles from './Form/Form';
+import Validation from '../ImageForm/Form/Form.Validation';
+// eslint-disable-next-line no-unused-vars
+import { IStaticImageForm } from '../../../../../TypeScript/Interfaces/IStaticImageForm';
+import myAxios from '../../../Utils/MyAxios';
+
+interface IProps {
+  myValues: IStaticImageForm
+}
+
+const MyImageBehavioursProfiles = withFormik<WithTranslation & WithSnackbarProps & IProps, IStaticImageForm>({
+  displayName: 'Static Content Image Form',
+  enableReinitialize: true,
+  mapPropsToValues: (props: IProps):IStaticImageForm => props.myValues,
+  validationSchema: Validation,
+  handleSubmit: async (values, { setSubmitting, props }): Promise<void> => {
+    const { enqueueSnackbar, t } = props;
+    await myAxios(window.location.href).post<IStaticImageForm>('Image/LogoEdit', {
+      Mime: values.Mime,
+      Alt_PT: values.Alt_PT,
+      Alt_ENG: values.Alt_ENG,
+      Src: values.Src,
+    }).then((response): void => {
+      const { data } = response;
+
+      if (data.Success) {
+        enqueueSnackbar(t('StaticImageForm:feedback.success'), {
+          variant: 'success',
+        });
+      } else {
+        enqueueSnackbar(t('StaticImageForm:feedback.failure'), {
+          variant: 'error',
+        });
+      }
+    }).catch((): void => {
+      enqueueSnackbar(t('StaticImageForm:feedback.failure'), {
+        variant: 'error',
+      });
+    });
+    setSubmitting(false);
+  },
+})(ImageBehavioursProfiles);
+
+export const Login = withTranslation()(withSnackbar(MyImageBehavioursProfiles));
+
+export default withTranslation()(
+  (props: WithTranslation & IProps): React.ReactElement<WithTranslation & IProps> => {
+    const { t, myValues } = props;
+    const classes = useStyles({});
+    setLanguage();
+
+    return (
+      <div className={classes.main}>
+        <Typography
+          gutterBottom
+          align="center"
+          variant="h5"
+        >
+          {t('FirstPageImages:title')}
+        </Typography>
+        <Login myValues={myValues} />
+      </div>
+    );
+  },
+);
