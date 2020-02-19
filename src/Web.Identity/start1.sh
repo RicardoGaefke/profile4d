14ef95f52dd0ed8a4a28a01130fbe97c7f5d46c1
Ricardo Gaefke <ricardogaefke@gmail.com>
2020-02-18 23:38:05 -0300

    Web.Identity node version

--- src/Web.Identity/start1.sh
+++ src/Web.Identity/start1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get install curl
-curl -sL https://deb.nodesource.com/setup_12.6.0 | bash
+curl -sL https://deb.nodesource.com/setup_12.x | bash
 apt-get install -y -qq nodejs
 
 set -e
