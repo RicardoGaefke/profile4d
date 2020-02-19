10c95f238b07e0a6f1a1168620953c82a2e00cfe
Ricardo Gaefke <ricardogaefke@gmail.com>
2020-02-19 00:44:24 -0300

    node nvm

--- src/Web.Identity/start1.sh
+++ src/Web.Identity/start1.sh
@@ -6,7 +6,7 @@ apt-get install curl
 # apt-get install -y -qq nodejs
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
-nvm install 12.6.0
+nvm install v12.6.0
 
 set -e
 export PORT=8080
