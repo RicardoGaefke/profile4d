0f50cdabe9c95fa12509b3a2242e314f9297d986
Ricardo Gaefke <ricardogaefke@gmail.com>
2020-02-20 20:43:55 -0300

    withContext

--- src/Web.Identity/React/Components/ConfigDrawer/Drawer.tsx
+++ src/Web.Identity/React/Components/ConfigDrawer/Drawer.tsx
@@ -3,11 +3,27 @@ import React from 'react';
 import { withTranslation, WithTranslation } from 'react-i18next';
 import { Drawer, Typography, Divider } from '@material-ui/core';
 import setLanguage from './Language';
-import { useStateValue } from '../../Initial/Context/StateProvider';
+import {
+  // eslint-disable-next-line no-unused-vars
+  useStateValue, withContext, IStateProvider, IContext,
+} from '../../Initial/Context/StateProvider';
 import Styles from './Styles';
 import ConfigTheme from './ChangeTheme/Theme';
 import ConfigLanguage from './ChangeLanguage/ChangeLanguage';
 import Button from './SaveButton/Button';
+
+const Ctx = withContext((props: any): React.ReactElement => {
+  // eslint-disable-next-line no-unused-vars
+  const { context } = props;
+
+  console.log('child', context);
+
+  return (
+    <>
+      {context[0].Title}
+    </>
+  );
+});
 
 export default withTranslation()(
   (props: WithTranslation): React.ReactElement<WithTranslation> => {
@@ -30,6 +46,7 @@ export default withTranslation()(
         onClose={toggle}
       >
         <div className={classes.root}>
+          <Ctx />
           <Typography variant="h6">
             {t('DrawerConfig:title')}
           </Typography>
