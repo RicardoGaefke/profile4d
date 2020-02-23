1f05ca4252fc86522ab14d6b364b7da8834d60a4
lucasneves.dev@gmail.com <lucasneves.dev@gmail.com>
2020-02-23 17:22:40 -0300

    InternalPartnerOne created

---
+++ src/Web.Admin/React/Components/FixedContent/InternalParnerOne/Page.tsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+// eslint-disable-next-line no-unused-vars
+import { IStaticIntroduction } from '../../../../../TypeScript/Interfaces/IStaticContent';
+import Loading from '../../Loading/Loading';
+import MyAxios from '../../../Utils/MyAxios';
+import MyInternalParnerOne from './InternalParnerOne';
+
+export default (): React.ReactElement => {
+  const [state, setState] = useState({} as IStaticIntroduction);
+
+  useEffect((): void => {
+    MyAxios(window.location.href)
+      .get<IStaticIntroduction>('StaticContent/InternalParnerOne')
+      .then((response): void => setState(response.data));
+  }, []);
+
+  return (
+    <>
+      {(!state.Success) ? (<Loading />) : (<MyInternalParnerOne myValues={state} />)}
+    </>
+  );
+};
