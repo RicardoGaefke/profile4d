2c126f98e6cd9738c82d3d5cdfb63b1bca5ea5f6
Ricardo Gaefke <ricardogaefke@gmail.com>
2020-02-29 02:34:02 -0300

    Questions + quantity

--- src/Web.Admin/React/Components/DynamicContent/Questions/Questions.tsx
+++ src/Web.Admin/React/Components/DynamicContent/Questions/Questions.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 // eslint-disable-next-line no-unused-vars
 import { withTranslation, WithTranslation } from 'react-i18next';
 import {
-  Typography,
+  Typography, Grid,
 } from '@material-ui/core';
+import Quantity from './Quantity/Quantity';
 import setLanguage from './Language';
 import useStyles from './Styles';
 
@@ -18,6 +19,16 @@ export default withTranslation()(
         <Typography variant="h4" align="center">
           {t('DynamicQuestions:title')}
         </Typography>
+        <Grid
+          justify="flex-end"
+          container
+        >
+          <Grid
+            item
+          >
+            <Quantity minimum={99} total={32} />
+          </Grid>
+        </Grid>
       </div>
     );
   },
