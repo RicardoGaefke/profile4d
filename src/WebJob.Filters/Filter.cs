eb40efcda92e37e4348639d0801f1aa62194cc8e
Ricardo Gaefke <ricardogaefke@gmail.com>
2020-06-18 10:16:23 -0300

    Adds WebJobs Filter

---
+++ src/WebJob.Filters/Filter.cs
@@ -0,0 +1,21 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace Profile4d.WebJob.Filters
+{
+  /// <summary>
+  /// Sample exception filter that shows how declarative error handling logic
+  /// can be integrated into the execution pipeline.
+  /// </summary>
+
+  public class ErrorHandlerAttribute : FunctionExceptionFilterAttribute
+  {
+    public override Task OnExceptionAsync(FunctionExceptionContext exceptionContext, CancellationToken cancellationToken)
+    {
+      exceptionContext.Logger.LogError($"ErrorHandler called. Function '{exceptionContext.FunctionName}:{exceptionContext.FunctionInstanceId} failed.");
+      return Task.CompletedTask;
+    }
+  }
+}
