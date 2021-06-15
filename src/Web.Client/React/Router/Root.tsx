eb9599e856cc7049fb0c1dfb0dab59d1d9be86ad
Ricardo Gaefke <ricardogaefke@gmail.com>
2021-06-15 09:57:58 -0300

    Adiciona recepção de chaves

--- src/Web.Client/React/Router/Root.tsx
+++ src/Web.Client/React/Router/Root.tsx
@@ -5,8 +5,9 @@ import MyAbout from '../Views/About/About';
 import MyNotFound from '../Views/404/404';
 import MyPolicy from '../Views/Policy/Policy';
 import AnswerRouter from '../Components/Answer/Router';
+import ConfirmKey from '../Components/ConfirmKey/ConfirmKey';
 
-export default (): React.ReactElement<any> => (
+const Router = (): JSX.Element => (
   <Switch>
     <Route exact path="/" component={MyHome} />
     <Route path="/home" component={MyHome} />
@@ -14,6 +15,9 @@ export default (): React.ReactElement<any> => (
     <Route path="/policy" component={MyPolicy} />
     <Route path="/404" component={MyNotFound} />
     <Route path="/answer/" component={AnswerRouter} />
+    <Route path="/confirmKey/:guid" component={ConfirmKey} />
     <Route component={MyNotFound} />
   </Switch>
 );
+
+export default Router;
