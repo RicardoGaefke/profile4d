001773d1e3df56f5c286d5ffbf5bb6956d326871
Ricardo Gaefke <ricardogaefke@gmail.com>
2021-07-26 18:03:58 -0300

    Adds Pagination component

---
+++ src/Web.Admin/React/Components/Pagination/Pagination.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router';
+import { storiesOf } from '@storybook/react';
+import { withConsole } from '@storybook/addon-console';
+// eslint-disable-next-line no-unused-vars
+import { action, HandlerFunction } from '@storybook/addon-actions';
+import MyThemeHoc from '../../Initial/Theme/StoryBookHOC';
+import Pagination from './Pagination';
+
+const App = (): React.ReactElement => (
+  <MyThemeHoc>
+    <Pagination
+      Page={2}
+      Last={5}
+      onChange={(page): void => { console.log(page); }}
+    />
+  </MyThemeHoc>
+);
+
+storiesOf('Pagination', module)
+  .addDecorator((story: any): React.ReactElement => (
+    <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
+  ))
+  .addDecorator((storyFn, context): React.ReactElement => withConsole()(storyFn)(context))
+  .add('Basic', (): React.ReactElement => (
+    <App />
+  ));
