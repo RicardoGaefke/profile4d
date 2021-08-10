0d81874fc0ce5bac9ef30f60ec84f0ac7252a368
Ricardo Gaefke <ricardogaefke@gmail.com>
2021-08-10 07:57:30 -0300

    Fixes Web.Admin UserAdmin.stories

--- src/Web.Admin/React/Components/UsersAdmin/UsersAdmin.stories.tsx
+++ src/Web.Admin/React/Components/UsersAdmin/UsersAdmin.stories.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 import { MemoryRouter } from 'react-router';
 import UsersAdmin from './UsersAdmin';
-import { IInitialContext } from '../../../../TypeScript/Interfaces/IInitialContext';
 import MyThemeHoc from '../../Initial/Theme/StoryBookHOC';
 
 export default {
@@ -11,21 +10,7 @@ export default {
   decorators: [(Component): JSX.Element => <MemoryRouter initialEntries={['/']}><Component /></MemoryRouter>],
 } as Meta;
 
-const contextDark = {
-  Ready: false,
-  Theme: 'dark',
-  Language: 'ENG',
-  ConsentCookie: true,
-  FirstName: '',
-  LastName: '',
-  Email: '',
-  KeepConnected: false,
-  Drawer: false,
-  IsAuthenticated: false,
-  Config: false,
-} as IInitialContext;
-
-const Template: Story = (args): JSX.Element => (
+const Template: Story = (): JSX.Element => (
   <MyThemeHoc>
     <>UsersAdmin</>
   </MyThemeHoc>
