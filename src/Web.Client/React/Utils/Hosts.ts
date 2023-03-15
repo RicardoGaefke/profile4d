5bfb1fb14b547aa5858a07f4320809a5eb0e5cd7
Ricardo Gaefke <ricardogaefke@gmail.com>
2023-03-15 16:47:37 -0300

    Atualiza axios

--- src/Web.Client/React/Utils/Hosts.ts
+++ src/Web.Client/React/Utils/Hosts.ts
@@ -11,9 +11,9 @@ export default class Hosts {
     if (this.Host.includes('localhost')) {
       myHost = 'https://localhost:5065/';
     } else if (this.Host.includes('staging')) {
-      myHost = 'https://api.staging.performance9x.com.br/';
+      myHost = 'https://api.staging.hextriade.com/';
     } else {
-      myHost = 'https://api.performance9x.com.br/';
+      myHost = 'https://api.hextriade.com/';
     }
 
     return myHost;
@@ -25,9 +25,9 @@ export default class Hosts {
     if (this.Host.includes('localhost')) {
       myHost = 'https://localhost:5050/';
     } else if (this.Host.includes('staging')) {
-      myHost = 'https://www.staging.performance9x.com.br/';
+      myHost = 'https://www.staging.hextriade.com/';
     } else {
-      myHost = 'https://www.performance9x.com.br/';
+      myHost = 'https://www.hextriade.com/';
     }
 
     return myHost;
@@ -39,9 +39,9 @@ export default class Hosts {
     if (this.Host.includes('localhost')) {
       myHost = 'https://localhost:5055/';
     } else if (this.Host.includes('staging')) {
-      myHost = 'https://identity.staging.performance9x.com.br/';
+      myHost = 'https://identity.staging.hextriade.com/';
     } else {
-      myHost = 'https://identity.performance9x.com.br/';
+      myHost = 'https://identity.hextriade.com/';
     }
 
     return myHost;
@@ -53,9 +53,9 @@ export default class Hosts {
     if (this.Host.includes('localhost')) {
       myHost = 'https://localhost:5060/';
     } else if (this.Host.includes('staging')) {
-      myHost = 'https://admin.staging.performance9x.com.br/';
+      myHost = 'https://admin.staging.hextriade.com/';
     } else {
-      myHost = 'https://admin.performance9x.com.br/';
+      myHost = 'https://admin.hextriade.com/';
     }
 
     return myHost;
